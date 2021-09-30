36483ff42380704792feb3ab465a5372725986ac
Sabri Bin Saep <sabrisaep@gmail.com>
2021-09-30 10:34:19 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ms">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+
+<body>
+    <h1>Hello world</h1>
+
+    <?php 
+    $nama = 'Ahammad';
+    $umur = 20 + 3;
+
+    echo "$nama berumur $umur tahun";
+    ?>
+</body>
+
+</html>
